eb61852d68ec8a9cd5b4920f5d81c33fa2b0059b
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2014-09-16 21:13:33 +0100

    Added Readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+On my 4 core 8 thread 3.5Ghz i7-3770K:
+
+Linux maxes out at 6881 Mb/sec with 2 thread and 1 buffer loopback @ 65507.
--- withgcc.sh
+++ withgcc.sh
@@ -1,1 +1,1 @@
-g++ -o test -O0 -g -std=c++11 -pthread main.cpp -lboost_system
+g++ -o test -O3 -g -std=c++11 -pthread main.cpp -lboost_system
