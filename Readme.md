e3574037f3d6d8f32168b7fb03f744582f5b9bca
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2014-09-17 17:01:49 +0100

    Updated for Windows.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
-On my 4 core 8 thread 3.5Ghz i7-3770K:
+On my 4 core 8 thread 3.5Ghz i7-3770K testing ASIO's ability to maximise UDP send and receive:
 
-Linux maxes out at 6881 Mb/sec with 2 thread and 1 buffer loopback @ 65507.
+### Loopback: ###
+Linux maxes out at 6881 Mb/sec with 2 thread and 1 buffer @ 65507.
+
+Windows maxes out at 251 Mb/sec with 2 thread and 4 buffer @ 65507.
+
+
+### Real NIC: ###
+Todo
--- main.cpp
+++ main.cpp
@@ -21,11 +21,12 @@ using asio::ip::udp;
 using boost::system::error_code;
 
 static std::atomic<bool> first_packet(true);
-static size_t buffers(4), packet_size(65507);
+static std::atomic<size_t> gate;
+static size_t threads(2/*std::thread::hardware_concurrency()*/), buffers(4), packet_size(65507);
 static udp::endpoint local(asio::ip::address_v4::any(), 7868), endpoint(asio::ip::address_v4::loopback(), 7868);
-static asio::io_service service;
-static udp::socket listening_socket(service);
 static std::chrono::time_point<std::chrono::high_resolution_clock> begin;
+static asio::io_service service(threads);
+static udp::socket listening_socket(service);
 
 #ifdef WIN32
 unsigned char *allocate_dma_buffer(size_t len) {
@@ -50,11 +51,14 @@ struct worker
 {
   udp::socket socket; // sockets aren't thread safe, so need one per thread
   udp::endpoint myremote; // need a local thread safe copy
-  size_t read_count, write_count;
+  size_t read_count, write_count, read_bytes;
   std::vector<unsigned char *> read_buffers, write_buffers;
   std::vector<unsigned char *>::iterator read_buffer, write_buffer;
   std::thread thread;
-  worker(size_t myidx) : socket(service, asio::ip::udp::v4()), myremote(endpoint), read_count(0), write_count(0), thread(&worker::run, this, myidx) { }
+  worker() : worker(0) { }
+  worker(size_t myidx) : socket(service, asio::ip::udp::v4()), myremote(endpoint), read_count(0), write_count(0), read_bytes(0), thread(&worker::run, this, myidx) { }
+  worker(const worker &) = delete;
+  worker(worker &&) : worker(0) {}
   void doread()
   {
     listening_socket.async_receive(asio::buffer(*read_buffer, packet_size), [&](error_code ec, size_t bytes)
@@ -67,6 +71,7 @@ struct worker
           begin=std::chrono::high_resolution_clock::now();
         }
         ++read_count;
+        read_bytes+=bytes;
       }
       else
         std::cout << "r " << ec << std::endl;
@@ -115,6 +120,9 @@ struct worker
     }
 #endif
     socket.connect(myremote);
+    --gate;
+    while(gate)
+      std::this_thread::yield();
     doread();
     dowrite();
     service.run();
@@ -151,6 +159,8 @@ int main(int argc, char *argv[])
     std::cerr << "Failed to open UDP socket\n";
     return 1;
   }
+  listening_socket.set_option(asio::socket_base::receive_buffer_size(65507));
+  listening_socket.set_option(asio::socket_base::send_buffer_size(65507));
   // Try to bind to this or next port
   if(listening_socket.bind(local, ec))
   {
@@ -169,27 +179,41 @@ int main(int argc, char *argv[])
   //getchar();
   
   std::vector<worker> workers;
-  workers.reserve(std::thread::hardware_concurrency());
+  workers.reserve(threads);
   std::cout << "Creating " << workers.capacity() << " workers to read and write\n";
   std::cout << "Press return to exit ...\n";
+  gate=workers.capacity()+1;
   for(size_t n=0; n<workers.capacity(); n++)
     workers.emplace_back(n);
+  auto temp=begin=std::chrono::high_resolution_clock::now();
+  while(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now()-temp).count()<3)
+    std::this_thread::yield();
+  --gate;
+  while(gate)
+    std::this_thread::yield();
+#if 0
   getchar();
+#else
+  // Windows blocks so heavily SpeedStep interferes so keep a CPU more busy ...
+  while(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now()-begin).count()<10)
+    std::this_thread::yield();
+#endif
   service.stop();
   for(auto &i: workers)
     i.join();
   auto end=std::chrono::high_resolution_clock::now();
   
   std::cout << "\nWorkers did:\n";
-  size_t reads=0, writes=0;
+  size_t reads=0, writes=0, bytes=0;
   for(auto &i: workers)
   {
     std::cout << "  " << i.read_count << " reads and " << i.write_count << " writes\n";
     reads+=i.read_count;
     writes+=i.write_count;
+    bytes+=i.read_bytes;
   }
-  std::cout << "Making a total of " << reads << " reads and " << writes << " writes\n";
-  double rate=reads*packet_size;
+  std::cout << "Making a total of " << reads << " reads and " << writes << " writes of " << bytes << " bytes\n";
+  double rate=bytes;
   rate/=std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count()/1000000000.0;
   rate/=1024*1024;
   std::cout << "And some " << rate << "Mb/sec.\n";
